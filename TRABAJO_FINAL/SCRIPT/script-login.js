a0ebbb075a297193896c86c0cfa9545850457038
Kevin Pazmiño Lino <kevinpazminolino17@gmail.com>
2026-07-16 13:56:31 -0500

    Add files via upload

--- TRABAJO_FINAL/SCRIPT/script-login.js
+++ TRABAJO_FINAL/SCRIPT/script-login.js
@@ -1,19 +1,36 @@
-window.addEventListener('load', function() {
-        const botonLogin = document.querySelector('.btn-login');
-        
-        if (botonLogin) {
-            if (localStorage.getItem('estaLogueado') === 'true') {
-                // Modificamos esta línea:
-                botonLogin.textContent = "Kevin (Cerrar Sesion)"; 
-                botonLogin.setAttribute('href', '#');  
-                
-                botonLogin.onclick = function() {
-                    localStorage.removeItem('estaLogueado');
-                    window.location.href = 'Index.html';
-                };
-            } else {
-                botonLogin.textContent = "Iniciar Sesión";
-                botonLogin.setAttribute('href', 'inicioSesion.html');
-            }
-        }
-    });
+window.addEventListener("load",()=>{
+const botones=document.querySelectorAll(".btn-login");
+
+    botones.forEach(boton=>{
+
+        if(localStorage.getItem("estaLogueado")==="true"){  
+            boton.textContent="👤 Kevin (Cerrar Sesión)";
+            boton.href="#";
+            boton.onclick=function(e){
+            e.preventDefault();
+            localStorage.removeItem("estaLogueado");
+            window.location.href="Index.html";
+
+};
+
+}else{
+    boton.textContent="👤 Iniciar Sesión";
+    boton.href="inicioSesion.html";
+    }
+});
+});
+
+function protegerPagina(){
+
+    if(localStorage.getItem("estaLogueado") !== "true"){
+        document.getElementById("modalLogin").style.display="flex";
+    }
+
+}
+function irLogin(){
+    window.location.href="inicioSesion.html";
+}
+
+function cerrarModal(){
+    window.location.href="Index.html";
+}
